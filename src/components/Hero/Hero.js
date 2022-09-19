665e973db03de5e6eea03ace4bd9c66c4202875c
Nicholas Huynh <nickhuynhq@gmail.com>
2022-09-18 20:36:21 -0400

    Minor Styling Changes

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import "./Hero.scss"
 import Cars from "../../assets/images/hero-image.png"
-import Sedan from "../../assets/images/honda-civic-2022.png"
 
 const Hero = () => {
   return (
     <section className="hero">
-      <img className="hero__cars" src={Cars}/>
+      <img className="hero__cars" src={Cars} alt="Truck and Sedan"/>
       <div className='hero__text-box'>
         <h2 className='hero__text-box-heading'>Want to know the true Cost of a Vehicle?</h2>
         <p className='hero__text-box-description'>See your true cost of ownership including  maintenance, insurance, gas, depreciation, and other fees!</p>
