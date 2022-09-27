fd00ebbcd765a4d825fe9a12797dd9d6b6370539
Nicholas Huynh <nickhuynhq@gmail.com>
2022-09-27 14:45:42 -0400

    Change base url for API and edit Image file path for Login

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = `http://localhost:8080`;
+// const BASE_URL = `http://localhost:8080`;
+const BASE_URL = `https://carma-island-api.herokuapp.com`;
 
 export const fetchCars = () => {
     return axios.get(`${BASE_URL}/cars/`);
